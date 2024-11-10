013b4ea6dbb7699e683fa0726b099fe70d7672c1
nirduani1 <nirduani@icloud.com>
2024-11-10 17:39:28 +0200

    done

--- NumWords.java
+++ NumWords.java
@@ -4,7 +4,7 @@ public class NumWords {
 		int ones = number%10 ;
 		int tens = ((number-ones)/10)%10 ;
 		int hundreds = (number-tens*10-ones)/100 ;
-		System.out.println("For the number: " + number);
+		System.out.println("For the number:" + number);
 		System.out.println(hundreds + " hundreds, "+ tens +" tens, and " + ones + " ones.");
 	}
 }
